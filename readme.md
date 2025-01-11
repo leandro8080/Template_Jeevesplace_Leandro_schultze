58891a1213a7bb4f7b2d492633e57c8d4b80caa9
leandro8080 <leandro.schultze@ict.csbe.ch>
2025-01-11 18:50:40 +0100

    Added function to execute querries
    Also Added an example to add a table.
    
    Fixed PGAdmin connection and added readme for instruction to access pgAdmin

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Template JeevesPlace Leandro Schultze
+
+## Start up
+
+## pgAdmin
+
+To access the database, use pgAdmin.
+
+### 1. Start PGAdmin
+
+When not already done, start the docker container with "docker-compose up"
+
+### 2. Add the server
+
+1. Open your webbrowser and go on localhost:9200
+2. Press on "Add New Server"
+    - **Host:** jeevesplace
+    - **Username:** jeevesplace
+    - **Password:** MegaSecret1234 (Unless you changed it)
+
+### 3. Access tables
+
+On the left, expand "Servers" -> "jeevesplace" -> "Databases" -> "jeevesplace" -> "Schemas" -> "Public" -> "tables"
--- server/database.js
+++ server/database.js
@@ -7,14 +7,33 @@ const dbConfig = new Client({
 	database: process.env.DB || "jeevesplace",
 });
 
+const createTablesQuery = `
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		firstname TEXT NOT NULL,
+		lastname TEXT NOT NULL
+	);`; // Insert more queries in this string;
+
 const initializeDatabase = async () => {
 	const client = new Client(dbConfig);
 	try {
 		await client.connect();
+		await queryDB(client, createTablesQuery); // Create tables
 		console.log("Connected to PostgreSQL database");
 		return client;
 	} catch (error) {
 		console.error("Error connecting to PostgreSQL database", error);
+		return;
+	}
+};
+
+const queryDB = async (db, query) => {
+	try {
+		const result = await db.query(query);
+		return result.rows;
+	} catch (error) {
+		console.error("Error executing query", error);
+		throw error;
 	}
 };
 
